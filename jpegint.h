ac30a1bf12751bd82e56158eb9456a28d9c086f3
DRC <dcommander@users.sourceforge.net>
2015-06-25 03:44:36 +0000

    Add a new libjpeg API function (jpeg_skip_scanlines()) to allow for partially decoding a JPEG image.
    git-svn-id: svn+ssh://svn.code.sf.net/p/libjpeg-turbo/code/trunk@1582 632fc199-4ca6-4c93-a231-07263d6284db

--- jpegint.h
+++ jpegint.h
@@ -6,6 +6,7 @@
  * Modified 1997-2009 by Guido Vollbeding.
  * libjpeg-turbo Modifications:
  * Copyright (C) 2015, D. R. Commander
+ * Copyright (C) 2015, Google, Inc.
  * For conditions of distribution and use, see the accompanying README file.
  *
  * This file provides common declarations for the various JPEG modules.
@@ -149,6 +150,12 @@ struct jpeg_decomp_master {
 
   /* State variables made visible to other modules */
   boolean is_dummy_pass;        /* True during 1st pass for 2-pass quant */
+
+  /* Buffer large enough to store an output row.  This is used when
+   * jpeg_skip_scanlines() chooses to "skip" a row by reading it into this
+   * dummy buffer.
+   */
+  JSAMPROW dummy_row_buffer;
 };
 
 /* Input control module */
