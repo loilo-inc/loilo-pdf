5963b3f7180807530abb652145a5e3d6455c4eba
Yusuke Sakurai <kerokerokerop@gmail.com>
2016-01-13 16:05:43 +0900

    fixed test application

--- app/src/main/java/tv/loilo/pdfiumandroid/MainActivity.java
+++ app/src/main/java/tv/loilo/pdfiumandroid/MainActivity.java
@@ -1,18 +1,25 @@
 package tv.loilo.pdfiumandroid;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import tv.loilo.pdfium.PdfRendererCompat;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -30,34 +37,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        File outFile = new File(getFilesDir(), "manual.pdf");
-        if (!outFile.exists()) {
-            try (InputStream is = getAssets().open("manual.pdf");
-                 FileOutputStream os = openFileOutput(outFile.getName(), Context.MODE_PRIVATE)
-            ) {
-                byte[] buffer = new byte[1024];
-                int len = 0;
-                while ((len = is.read(buffer)) > 0) {
-                    os.write(buffer,0,len);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        try (ParcelFileDescriptor fd = ParcelFileDescriptor.open(outFile, ParcelFileDescriptor.MODE_READ_ONLY);
-             PdfRendererCompat renderer = new PdfRendererCompat(fd);
-             PdfRendererCompat.Page page1 = renderer.openPage(0)
+        File tmp = new File(getCacheDir(),"manual.pdf");
+        try(InputStream ins = getAssets().open("manual.pdf");
+            BufferedInputStream bis = new BufferedInputStream(ins);
+            FileOutputStream bos = new FileOutputStream(tmp);
+            BufferedOutputStream bfw = new BufferedOutputStream(bos)
         ){
-            assert renderer.getPageCount() == 38;
-            Bitmap out = Bitmap.createBitmap(page1.getWidth(), page1.getHeight(), Bitmap.Config.ARGB_8888);
-            Matrix x2 = new Matrix();
-            x2.setScale(2,2);
-            page1.render(out, null, x2, PdfRendererCompat.Page.RENDER_MODE_FOR_DISPLAY);
-            imageView.setImageBitmap(out);
+            // tmp copy
+            final byte[] buffer = new byte[256];
+            int read = 0;
+            long len = 0;
+            while((read = bis.read(buffer)) != -1) {
+                bfw.write(buffer,0,read);
+                len += read;
+            }
+            bfw.flush();
+            try (ParcelFileDescriptor fd = ParcelFileDescriptor.open(tmp,ParcelFileDescriptor.MODE_READ_ONLY);
+                 PdfRendererCompat renderer = new PdfRendererCompat(fd);
+                 PdfRendererCompat.Page page1 = renderer.openPage(1)
+            ) {
+                assert renderer.getPageCount() == 38;
+                Bitmap out = Bitmap.createBitmap(page1.getWidth(), page1.getHeight(), Bitmap.Config.ARGB_8888);
+                Matrix x2 = new Matrix();
+                x2.setScale(2,2);
+                page1.render(out, null, x2, PdfRendererCompat.Page.RENDER_MODE_FOR_DISPLAY);
+                imageView.setImageBitmap(out);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
