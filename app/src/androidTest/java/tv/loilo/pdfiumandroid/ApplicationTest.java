ffe5cf594ce42feeedf9d7b2ebba6e7946cb5b08
hayama-junpei <hayama@loilo.tv>
2016-12-11 02:29:44 +0900

    libpdfium.soファイルが出力に含まれるようにした。 android sdk 更新。

--- app/src/androidTest/java/tv/loilo/pdfiumandroid/ApplicationTest.java
+++ app/src/androidTest/java/tv/loilo/pdfiumandroid/ApplicationTest.java
@@ -1,13 +1,10 @@
 package tv.loilo.pdfiumandroid;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
 }
